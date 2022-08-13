1a435a5a1858bae9e981822032d454c955486e4d
harshmangalam <harshdev8218@gmail.com>
2022-08-13 07:02:51 +0530

    test chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <Button>
+      Supabase
+    </Button>
+  )
 };
 
 export default Home;
