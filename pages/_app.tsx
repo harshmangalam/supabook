36bb61485186f2d8956ab25c8d7c5ffecdf92eed
harshmangalam <harshdev8218@gmail.com>
2022-08-13 07:00:53 +0530

    added theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
