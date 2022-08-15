922c10ace82424f1bd18cec8d12dc10a7fcb9ec9
harshmangalam <harshdev8218@gmail.com>
2022-08-15 14:01:52 +0530

    added auth signup

--- pages/auth/login-with-email.tsx
+++ pages/auth/login-with-email.tsx
@@ -11,6 +11,7 @@ import {
   useColorModeValue,
   Container,
   FormErrorMessage,
+  useToast,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -26,6 +27,7 @@ const schema = yup
   .required();
 
 export default function LoginWithEmailRoute() {
+  const toast = useToast();
   const {
     handleSubmit,
     register,
@@ -34,71 +36,84 @@ export default function LoginWithEmailRoute() {
 
   const onSubmit = async ({ email, password }) => {
     try {
-      const { user, error } = await supabase.auth.signIn({
+      const { user, error } = await supabase.auth.signUp({
         email,
         password,
       });
 
-      console.log(user);
-      console.log(error);
+      if (user) {
+        toast({
+          title: "Authentication",
+          description: "You have logged in successfully",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+
+      if (error) {
+        toast({
+          title: "Authentication Error",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
     } catch (error) {
       console.log(error);
     }
   };
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Container>
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-          <Stack align={"center"}>
-            <Heading fontSize={"4xl"}>Authenticate</Heading>
-            <Text fontSize={"lg"} color={"gray.600"}>
-              Login With Email
-            </Text>
-          </Stack>
-
-          <Box
-            rounded={"lg"}
-            bg={useColorModeValue("white", "gray.700")}
-            boxShadow={"lg"}
-            p={8}
-          >
-            <Stack as="form" onSubmit={handleSubmit(onSubmit)} spacing={4}>
-              <FormControl id="email" isInvalid={errors.email}>
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" {...register("email")} />
-                {errors?.email && (
-                  <FormErrorMessage>{errors.email.message}</FormErrorMessage>
-                )}
-              </FormControl>
-              <FormControl id="password" isInvalid={errors.password}>
-                <FormLabel>Password</FormLabel>
-                <Input type="password" {...register("password")} />
-                {errors?.password && (
-                  <FormErrorMessage>{errors.password.message}</FormErrorMessage>
-                )}
-              </FormControl>
-
-              <Button
-                isLoading={isSubmitting}
-                type="submit"
-                colorScheme="green"
-              >
-                Sign in
-              </Button>
-            </Stack>
-            <Link href={"/auth"} passHref>
-              <Button as="a" colorScheme="twitter" size="sm" mt={3} w="full">
-                View other login options
-              </Button>
-            </Link>
-          </Box>
+    <Container>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Authenticate</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            Login With Email
+          </Text>
         </Stack>
-      </Container>
-    </Flex>
+
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack as="form" onSubmit={handleSubmit(onSubmit)} spacing={4}>
+            <FormControl id="email" isInvalid={errors.email}>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" {...register("email")} />
+              {errors?.email && (
+                <FormErrorMessage>{errors.email.message}</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl id="password" isInvalid={errors.password}>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" {...register("password")} />
+              {errors?.password && (
+                <FormErrorMessage>{errors.password.message}</FormErrorMessage>
+              )}
+            </FormControl>
+
+            <Button isLoading={isSubmitting} type="submit" colorScheme="green">
+              Sign in
+            </Button>
+          </Stack>
+          <Link href={"/auth"} passHref>
+            <Button
+              as="a"
+              variant={"link"}
+              colorScheme="twitter"
+              size="sm"
+              mt={3}
+              w="full"
+            >
+              View other login options
+            </Button>
+          </Link>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
