7d27ccb8870a3c76145ab2dcf66488ae67b4574a
harshmangalam <harshdev8218@gmail.com>
2022-08-15 12:47:17 +0530

    added friends

---
+++ pages/friends/index.tsx
@@ -0,0 +1,65 @@
+import {
+  Button,
+  Container,
+  Grid,
+  GridItem,
+  Icon,
+  VStack,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import {
+  RiUserFollowLine,
+  RiUserLocationLine,
+  RiUserShared2Line,
+  RiUserReceived2Line,
+} from "react-icons/ri";
+export default function FriendsRoute() {
+  return (
+    <Container maxW={"container.lg"}>
+      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+        <GridItem colSpan={2}>
+          <VStack align={"start"}>
+            {tabs.map((tab) => (
+              <Link href={tab.href} key={tab.name} passHref>
+                <Button
+                  as="a"
+                  leftIcon={
+                    <Icon fontSize={22} color="green.500" as={tab.icon} />
+                  }
+                  variant={"ghost"}
+                  rounded="full"
+                >
+                  {tab.name}
+                </Button>
+              </Link>
+            ))}
+          </VStack>
+        </GridItem>
+        <GridItem colSpan={4}></GridItem>
+      </Grid>
+    </Container>
+  );
+}
+
+const tabs = [
+  {
+    name: "My Friends",
+    href: "/friends",
+    icon: RiUserFollowLine,
+  },
+  {
+    name: "Suggestions",
+    href: "/friends/suggestions",
+    icon: RiUserLocationLine,
+  },
+  {
+    name: "Friend Requests Sent",
+    href: "/friends/friend-requests-sent",
+    icon: RiUserShared2Line,
+  },
+  {
+    name: "Friend Requests Received",
+    href: "/friends/friend-requests-received",
+    icon: RiUserReceived2Line,
+  },
+];
