400a69104f164294f5661747f0866da4b4844408
harshmangalam <harshdev8218@gmail.com>
2022-08-15 13:33:33 +0530

    update auth screen

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Flex,
   Box,
   Stack,
   Heading,
@@ -12,37 +11,34 @@ import {
 import Link from "next/link";
 export default function AuthRoute() {
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Container>
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-          <Stack align={"center"}>
-            <Heading fontSize={"4xl"}>Authenticate</Heading>
-            <Text fontSize={"lg"} color={"gray.600"}>
-              Sign in using different type of authentication providers
-            </Text>
-          </Stack>
-          <Box
-            rounded={"lg"}
-            bg={useColorModeValue("white", "gray.700")}
-            boxShadow={"lg"}
-            p={4}
-          >
-            <SimpleGrid columns={2} spacing={4}>
-              <Link href="/auth/login-with-email" passHref>
-                <Button as="a" colorScheme="green">Login With Email</Button>
-              </Link>
-              <Link href="/auth/login-with-magic-link" passHref>
-                <Button as="a" colorScheme="green">Login With Maginc Link</Button>
-              </Link>
-            </SimpleGrid>
-          </Box>
+    <Container>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Authenticate</Heading>
+          <Text fontSize={"lg"} color={"gray.600"} maxW="md" textAlign={"center"}>
+            Sign in your account using different type of authentication providers provided by supabase
+          </Text>
         </Stack>
-      </Container>
-    </Flex>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={4}
+        >
+          <SimpleGrid columns={2} spacing={4}>
+            <Link href="/auth/login-with-email" passHref>
+              <Button as="a" colorScheme="green">
+                Login With Email
+              </Button>
+            </Link>
+            <Link href="/auth/login-with-magic-link" passHref>
+              <Button as="a" colorScheme="green">
+                Login With Maginc Link
+              </Button>
+            </Link>
+          </SimpleGrid>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
