57d053d738836916cdd32494c262deba93df855e
harshmangalam <harshdev8218@gmail.com>
2022-08-16 08:30:27 +0530

    updated signin

--- pages/auth/signin/index.tsx
+++ pages/auth/signin/index.tsx
@@ -5,8 +5,8 @@ import {
     Text,
     useColorModeValue,
     Container,
-    SimpleGrid,
     Button,
+    HStack,
   } from "@chakra-ui/react";
   import Link from "next/link";
   export default function AuthSigninRoute() {
@@ -14,29 +14,30 @@ import {
       <Container>
         <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
           <Stack align={"center"}>
-            <Heading fontSize={"4xl"}>Authenticate</Heading>
+            <Heading fontSize={"4xl"}>Signin</Heading>
             <Text fontSize={"lg"} color={"gray.600"} maxW="md" textAlign={"center"}>
-              Sign in your account using different type of authentication providers provided by supabase
+              Signin your account using different type of authentication providers
             </Text>
           </Stack>
           <Box
             rounded={"lg"}
             bg={useColorModeValue("white", "gray.700")}
-            boxShadow={"lg"}
-            p={4}
+            boxShadow={"md"}
+            borderWidth={"2px"}
+            p={6}
           >
-            <SimpleGrid columns={2} spacing={4}>
+            <HStack>
               <Link href="/auth/signin/signin-with-email" passHref>
-                <Button as="a" colorScheme="green">
+                <Button as="a" colorScheme="purple">
                   Signin With Email
                 </Button>
               </Link>
               <Link href="/auth/signin/signin-with-magic-link" passHref>
-                <Button as="a" colorScheme="green">
+                <Button as="a" colorScheme="pink">
                   Signin With Maginc Link
                 </Button>
               </Link>
-            </SimpleGrid>
+            </HStack>
           </Box>
         </Stack>
       </Container>
