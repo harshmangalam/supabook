211934cb9b498740a378f02c08749b34a74a9d94
harshmangalam <harshdev8218@gmail.com>
2022-08-17 23:02:10 +0530

    added profile specific posts

--- pages/[profileId]/index.tsx
+++ pages/[profileId]/index.tsx
@@ -1,5 +1,31 @@
+import { Container, SimpleGrid } from "@chakra-ui/react";
+import Post from "../../components/Post";
 import ProfileLayout from "../../layouts/ProfileLayout";
+import { supabase } from "../../utils/supabaseClient";
 
-export default function Profile() {
-  return <ProfileLayout>Home</ProfileLayout>;
+export default function Home({ posts }) {
+  return (
+    <ProfileLayout>
+      {/* posts  */}
+      <Container>
+        <SimpleGrid spacing={4}>
+          {posts.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </ProfileLayout>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const { data } = await supabase
+    .from("post")
+    .select(`*,author(*)`)
+    .eq("author", params.profileId);
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
