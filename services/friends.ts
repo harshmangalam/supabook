ecbc87750322bc5e17036e1f28429a466c9e0138
harshmangalam <harshdev8218@gmail.com>
2022-08-18 15:39:43 +0530

    added friend request sent service

---
+++ services/friends.ts
@@ -0,0 +1,11 @@
+import { supabase } from "../utils/supabaseClient";
+
+export async function fetchFriendRequestSent(profileId: string) {
+  const { data, error } = await supabase
+    .from("friend_request")
+    .select("*")
+    .eq("from", profileId);
+
+  if (error) throw error;
+  if (data) return data[0];
+}
